140cc1d6058448bc8b7cc8003ccfc77f6f54c9d2
Ashley <ashley.martinez32@gmail.com>
2023-05-09 04:39:46 -0500

    working on player turn

--- react-tictactoe/js/app.js
+++ react-tictactoe/js/app.js
@@ -55,6 +55,16 @@ const App = {
         square.replaceChildren(icon);
 
         // check if game is a win or tie
+        const winningPatterns = [
+          [1, 2, 3],
+          [1, 5, 9],
+          [1, 4, 7],
+          [2, 5, 8],
+          [3, 5, 7],
+          [3, 6, 9],
+          [4, 5, 6],
+          [7, 8, 9],
+        ];
       });
     });
   },
