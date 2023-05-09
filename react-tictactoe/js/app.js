bc1e7b9c2822cdbecb57e2eebb4e307d2f360d85
Ashley <ashley.martinez32@gmail.com>
2023-05-09 03:25:58 -0500

    added event listeners and registerEvent funt

--- react-tictactoe/js/app.js
+++ react-tictactoe/js/app.js
@@ -1,13 +1,35 @@
 const App = {
   // all of the selected HTML elements
   $: {
-    actions: document.querySelector(".actions"),
-    actionsItems: document.querySelector(".items"),
+    actions: document.querySelector('[data-id="actions"]'),
+    actionsItems: document.querySelector('[data-id="actions-items"]'),
+    resetBtn: document.querySelector('[data-id="reset-btn"]'),
+    newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+    squares: document.querySelectorAll('[data-id="square"]'),
   },
 
   init() {
+    App.registerEventListeners();
+  },
+
+  //   function created to use in int to runa ll the event listeners
+  registerEventListeners() {
     App.$.actions.addEventListener("click", (event) => {
       App.$.actionsItems.classList.toggle("hidden");
+    });
+
+    App.$.resetBtn.addEventListener("click", (event) => {
+      console.log("reset stuff:");
+    });
+
+    App.$.newRoundBtn.addEventListener("click", (event) => {
+      console.log("new round");
+    });
+
+    App.$.squares.forEach((square) => {
+      square.addEventListener("click", (event) => {
+        console.log("this is the id of the square clicked: ", event.target.id);
+      });
     });
   },
 };
