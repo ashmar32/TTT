cdf558425788e2940bf91ad4de1ddcb44f53fc75
Ashley <ashley.martinez32@gmail.com>
2023-05-09 02:51:10 -0500

    refactored toggle and added initializer

--- react-tictactoe/js/app.js
+++ react-tictactoe/js/app.js
@@ -1,9 +1,15 @@
-// get the div element .actions and .items
-const actions = document.querySelector(".actions");
-// actionsItems gets the div from the above created actions query
-const actionsItems = actions.querySelector(".items");
-// addEventListener
-actions.addEventListener("click", (event) => {
-  // this allows us to toggle the hidden class each time the click happens from the event listener on the actions class
-  actionsItems.classList.toggle("hidden");
-});
+const App = {
+  // all of the selected HTML elements
+  $: {
+    actions: document.querySelector(".actions"),
+    actionsItems: document.querySelector(".items"),
+  },
+
+  init() {
+    App.$.actions.addEventListener("click", (event) => {
+      App.$.actionsItems.classList.toggle("hidden");
+    });
+  },
+};
+
+window.addEventListener("load", App.init);
